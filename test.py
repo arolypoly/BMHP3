cba866f2bd07efa209181a8558c5934a9256799d
Will Dang <dangwilliam2@gmail.com>
2018-02-16 10:53:54 -0800

    Final testing. Moving on now. Unless something breaks.

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import dxlcore
+import time
+
+import keyboard
+
 from dxl.dxlchain import DxlChain
 
 # Open the serial device
@@ -7,7 +10,26 @@ chain = DxlChain("/dev/ttyUSB0", rate=1000000)
 # Load all the motors and obtain the list of IDs
 motors = chain.get_motor_list(broadcast=True)  # Discover all motors on the chain and return their IDs
 chain.dump()
-chain.goto(0, 0, speed=100, blocking=True)
-chain.goto(0, 4000, speed=0, blocking=True)
-print(chain.get_reg(0, "present_load"))
-chain.disable()
+chain.goto(2, 0, speed=100, blocking=True)
+chain.goto(2, 1000, speed=0, blocking=True)
+print(chain.get_reg(2, "present_load"))
+
+
+def periodic(func, **kwargs):
+    starttime = time.time()
+    while True:
+        func(**kwargs)
+        time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+
+n = 0
+while True:
+    if keyboard.is_pressed('a'):
+        chain.goto(2, n.__add__(1), speed=100, blocking=False)
+    elif keyboard.is_pressed('d'):
+        chain.goto(2, n.__sub__(1), speed=100, blocking=False)
+    elif keyboard.is_pressed('space'):
+        chain.disable()
+        break
+    else:
+        time.sleep(.001)
