91da5b9ca685076d2037b35849e5832d4d526626
Will Dang <dangwilliam2@gmail.com>
2018-02-23 16:33:27 -0800

    Attempt number one.

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+import logging
 import time
 
 import keyboard
+import sys
+
+import myo
+import classify_myo
 
 from dxl.dxlchain import DxlChain
 
@@ -15,20 +20,41 @@ chain.goto(2, 1000, speed=0, blocking=True)
 print(chain.get_reg(2, "present_load"))
 
 
-def periodic(func, **kwargs):
-    starttime = time.time()
-    while True:
-        func(**kwargs)
-        time.sleep(60.0 - ((time.time() - starttime) % 60.0))
-
-n = 0
-while True:
-    if keyboard.is_pressed('a'):
-        chain.goto(2, n.__add__(1), speed=100, blocking=False)
-    elif keyboard.is_pressed('d'):
-        chain.goto(2, n.__sub__(1), speed=100, blocking=False)
-    elif keyboard.is_pressed('space'):
-        chain.disable()
-        break
+def myo2dyna(pose):
+    if pose.__eq__("rest"):
+        chain.goto(2, 0, speed=100, blocking=False)
+    elif pose.__eq__("fingersSpread"):
+        chain.goto(2, 1000, speed=100, blocking=False)
     else:
-        time.sleep(.001)
+        logging.error("Invalid pose.")
+
+
+m = myo.Myo(myo.NNClassifier(), sys.argv[1] if len(sys.argv) >= 2 else None)
+hnd = classify_myo.EMGHandler(m)
+m.add_emg_handler(hnd)
+m.add_arm_handler(lambda arm, xdir: print('arm', arm, 'xdir', xdir))
+m.add_pose_handler(lambda p: print('pose', p))
+m.add_pose_handler(lambda p: myo2dyna(p))
+try:
+    m.connect()
+    while True:
+        m.run(1)
+except RuntimeError:
+    logging.error("Oof.")
+except KeyboardInterrupt:
+    logging.info("Stopping...")
+finally:
+    m.disconnect()
+    logging.info("Have nice day.")
+
+# n = 0
+# while True:
+#    if keyboard.is_pressed('a'):
+#        chain.goto(2, n.__add__(1), speed=100, blocking=False)
+#    elif keyboard.is_pressed('d'):
+#        chain.goto(2, n.__sub__(1), speed=100, blocking=False)
+#    elif keyboard.is_pressed('space'):
+#        chain.disable()
+#        break
+#    else:
+#        time.sleep(.001)
